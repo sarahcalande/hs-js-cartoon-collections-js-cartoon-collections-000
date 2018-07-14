30cd072ae0c91c36a0c7af21189b7cc287218adb
scalande@yahoo.com <scalande@yahoo.com>
2018-07-14 11:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 function dwarfRollCall(dwarves) {
+  var i = 0; i < dwarves.length; i++;
+  return `${i}. ${dwarves[0]} ${i+1}. ${dwarves[1]} ${i+2}. ${dwarves[2]}`
 }
 
 function summonCaptainPlanet(planeteerCalls){
-}
-
+   var newPlanet = [];
+   for(var i = 0; i < planeteerCalls.length; i++) {
+     newPlanet.push(planeteerCalls[i].toUpperCase() + '!');
+   }
+   return newPlanet; 
+ }
+ 
 function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
+  	var cheese = ["camembert", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+		var cheeseIdx = cheese.indexOf(foods[i]);
+		if(cheeseIdx !== -1) {
+			return foods[i];
+		}
+	}
+	return "no cheese!";
+
 }
